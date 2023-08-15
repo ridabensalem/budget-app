ff20d975241095e5dc4498e3e4602fcb17023fc4
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-15 13:53:17 +0100

    Add authentication properities to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :groups, foreign_key: 'user_id', dependent: :destroy
   has_many :entities, foreign_key: 'user_id', dependent: :destroy
   validates :name, presence: true
