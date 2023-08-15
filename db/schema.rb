31a17e9a7fd8a356d16509fbbada59995b8c46fd
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-15 13:52:40 +0100

    Set up devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_101759) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_112358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,15 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_101759) do
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_entities_on_group_id"
     t.index ["user_id"], name: "index_entities_on_user_id"
-  end
-
-  create_table "group_entities", force: :cascade do |t|
-    t.bigint "groups_id", null: false
-    t.bigint "entities_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["entities_id"], name: "index_group_entities_on_entities_id"
-    t.index ["groups_id"], name: "index_group_entities_on_groups_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -47,11 +38,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_101759) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users"
-  add_foreign_key "group_entities", "entities", column: "entities_id"
-  add_foreign_key "group_entities", "groups", column: "groups_id"
   add_foreign_key "groups", "users"
 end
