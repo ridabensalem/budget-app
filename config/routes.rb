2b998c3b42945afd0202157ca1ad02622e3d48f7
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-16 19:40:58 +0100

    Update budget app project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
+  get '/home', to: 'home#index', as: 'home'
+  
   devise_for :users 
   resources :users, only: [:index, :show] do
     resources :groups do
-      resources :entities
+      resources :entities, only: [:new, :create]
     end
   end
   
-end
+  root 'groups#index'
+  end
